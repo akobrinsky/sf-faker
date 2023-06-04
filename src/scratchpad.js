c95eba3dd31756ecd1a7861ca53d47bafcbe9cb5
Aryeh Kobrinsky <shmaryeh@gmail.com>
2023-06-04 15:19:40 -0400

    write to csv

--- src/scratchpad.js
+++ src/scratchpad.js
@@ -1,34 +1,88 @@
 import { faker } from '@faker-js/faker'
-import { cloneDeep, difference } from 'lodash'
+import { format } from '@fast-csv/format'
+import { appendFileSync, writeFileSync } from 'fs'
+const fileName = 'accounts.csv';
+const csvFile = fs.createWriteStream(fileName);
+const stream = format({ headers:true });
+stream.pipe(csvFile);
+
+const buildDomain = (accountName) => {
+  const suffix = faker.internet.domainSuffix()
+  const domainName = accountName.split(/[\' ,-]+/).join('-').toLowerCase()
+  return `${domainName}.${suffix}`
+}
+
 function createRandomAccount() {
+  const accountTypes = ['Customer - Foo', 'Customer - Channel', 'Customer - Direct']
+  const name = faker.company.name()
+  const email = faker.internet.email({
+    allowSpecialCharacters: false,
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    provider: 'google.com'
+  })
+  
   return {
-    userId: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
-    name: faker.company.name(),
-    url: faker.internet.domainName()
+    name,
+    domain: buildDomain(name),
+    type: faker.helpers.arrayElement(accountTypes),
+    email
   };
 }
 
-const seedFirst1000 = (amount, arr) => {
-  faker.seed(132)
+const seedAccountsHelper = (amount, arr, seed) => {
+  faker.seed(seed)
   for (let i = 0; i < amount; i += 1) {
     arr.push(createRandomAccount())
   }
 }
 
-faker.seed(132)
+class User {
+  constructor({name, id, type}) {
+    this.name = name
+    this.id = id
+    this.type = type
+  }
+}
+
 const accounts = []
 const accountsDeux = []
 const accountsTrois = []
-seedFirst1000(1000, accounts)
-seedFirst1000(1000, accountsDeux)
-seedFirst1000(1000, accountsTrois)
-accounts[0].username
-accountsDeux[0].username
-accountsTrois[0].username
-console.log(accounts[9].url);
+
+const TOTAL_ACCOUNTS = 1000
+const FORTY_PERCENT_MATCH = TOTAL_ACCOUNTS * 0.4
+const THIRTY_PERCENT_MATCH = TOTAL_ACCOUNTS * 0.3
+
+console.log(TOTAL_ACCOUNTS,FORTY_PERCENT_MATCH, THIRTY_PERCENT_MATCH);
+
+// All have first 15,0000
+seedAccountsHelper(TOTAL_ACCOUNTS, accounts, 0)
+seedAccountsHelper(FORTY_PERCENT_MATCH, accountsDeux, 0)
+seedAccountsHelper(THIRTY_PERCENT_MATCH, accountsTrois, 0)
+
+// Fill in the rest with seed after match points
+seedAccountsHelper(TOTAL_ACCOUNTS - FORTY_PERCENT_MATCH, accountsDeux, FORTY_PERCENT_MATCH)
+seedAccountsHelper(TOTAL_ACCOUNTS - THIRTY_PERCENT_MATCH, accountsTrois, THIRTY_PERCENT_MATCH)
+
+accounts.forEach((account, idx) => {
+  const {name, domain, type, email} = account
+  stream.write({name, domain, type, email})
+});
+
+stream.end()
+
+accountsDeux.forEach((account, idx) => {
+  const {name, domain, type} = account
+  const csv = `${name},${domain},${type}\n`
+  appendFileSync(`./accountsDeux.csv`, csv);
+});
+
+accountsTrois.forEach((account, idx) => {
+  const {name, domain, type} = account
+  const csv = `${name},${domain},${type}\n`
+  appendFileSync(`./accountsTrois.csv`, csv);
+});
+
+for (let i = 0; i < 10; i++) {
+  console.log(faker.commerce.product())
+}

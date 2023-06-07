bf5f7503a68e692d6a0ad2f1bc3a0a10b914d49f
Aryeh Kobrinsky <shmaryeh@gmail.com>
2023-06-07 11:23:04 -0400

    make all files

--- src/scratchpad.js
+++ src/scratchpad.js
@@ -1,32 +1,27 @@
 import { faker } from '@faker-js/faker';
 import { format } from '@fast-csv/format';
-import fs from 'fs';
-import companies from './real-companies.js'
+import { createWriteStream } from 'fs';
+import companies from './real-companies.js';
 
-
-const fileName = 'accounts.csv';
-const csvFile = fs.createWriteStream(fileName);
-const stream = format({ headers: true });
-stream.pipe(csvFile);
-
+const USER_IDS = []
 function createRandomAccount() {
-  const accountTypes = [ 
+  const accountTypes = [
     'Prospect',
     'Customer - Channel',
     'Customer - Direct',
     'Technology Partner',
     'Other',
   ];
-  const company = faker.helpers.arrayElement(companies)
+  const company = faker.helpers.arrayElement(companies);
   const ratingTypes = ['Hot', 'Warm', 'Cold'];
   const slaTypes = ['Gold', 'Silver', 'Platinum', 'Bronze'];
   const CustomerPriority__c = ['High', 'Low', 'Medium'];
   const UpsellOpportunity__c = ['Maybe', 'No', 'Yes'];
 
-  const billingStreet =  faker.location.streetAddress(false);
-  const billingCity =  faker.location.city();
-  const billingState =  faker.location.state();
-  const billingPostalCode =  faker.location.zipCode();
+  const billingStreet = faker.location.streetAddress(false);
+  const billingCity = faker.location.city();
+  const billingState = faker.location.state();
+  const billingPostalCode = faker.location.zipCode();
   const billingCountry = 'United States';
 
   return {
@@ -52,30 +47,6 @@ function createRandomAccount() {
   };
 }
 
-function buildOpps(amount, accountName) {
-  const result = [];
-  const types = ['foo', 'bar', 'baz'];
-  const stages = [
-    'Prospecting',
-    'Qualification',
-    'Needs Analysis',
-    'Value Proposition',
-    'Perception Analysis',
-    'Closed Won',
-    'Closed Lost',
-  ];
-  for (let i = 0; i < amount; i += 1) {
-    result.push({
-      accountName,
-      type: faker.helpers.arrayElement(types),
-      stage: faker.helpers.arrayElement(stages),
-      opportunityName: `${accountName} - ${faker.helpers.arrayElement(types)}`,
-      amount: faker.finance.amount(50000, 400000, 0),
-    });
-  }
-  return result;
-}
-
 const seedAccountsHelper = (amount, arr, seed) => {
   faker.seed(seed);
   for (let i = 0; i < amount; i += 1) {
@@ -83,33 +54,42 @@ const seedAccountsHelper = (amount, arr, seed) => {
   }
 };
 
-const accounts = [];
-const accountsDeux = [];
-const accountsTrois = [];
+const accountsLookup = {
+  one: [],
+  two: [],
+  three: []
+}
 
 const TOTAL_ACCOUNTS = 500;
-const FORTY_PERCENT_MATCH = TOTAL_ACCOUNTS * 0.4;
-const THIRTY_PERCENT_MATCH = TOTAL_ACCOUNTS * 0.3;
+const TOTAL_ACCOUNT_TWO = TOTAL_ACCOUNTS * 0.4;
+const TOTAL_ACCOUNT_THREE = TOTAL_ACCOUNTS * 0.3;
 
-// All have first 15,0000
-seedAccountsHelper(TOTAL_ACCOUNTS, accounts, 0);
-seedAccountsHelper(FORTY_PERCENT_MATCH, accountsDeux, 0);
-seedAccountsHelper(THIRTY_PERCENT_MATCH, accountsTrois, 0);
+seedAccountsHelper(TOTAL_ACCOUNTS, accountsLookup.one, 0);
+seedAccountsHelper(TOTAL_ACCOUNT_TWO, accountsLookup.two, 0);
+seedAccountsHelper(TOTAL_ACCOUNT_THREE, accountsLookup.three, 0);
 
 // Fill in the rest with seed after match points
 seedAccountsHelper(
-  TOTAL_ACCOUNTS - FORTY_PERCENT_MATCH,
-  accountsDeux,
-  FORTY_PERCENT_MATCH
+  TOTAL_ACCOUNTS - TOTAL_ACCOUNT_TWO,
+  accountsLookup.two,
+  TOTAL_ACCOUNT_TWO
 );
 seedAccountsHelper(
-  TOTAL_ACCOUNTS - THIRTY_PERCENT_MATCH,
-  accountsTrois,
-  THIRTY_PERCENT_MATCH
+  TOTAL_ACCOUNTS - TOTAL_ACCOUNT_THREE,
+  accountsLookup.three,
+  TOTAL_ACCOUNT_THREE
 );
 
-accounts.forEach((account, idx) => {
-  stream.write(account);
-});
 
-stream.end();
+for (const file of ['one', 'two', 'three']) {
+  const fileName = `accounts-${file}.csv`
+  const csvFile = createWriteStream(fileName);
+  const stream = format({ headers: true });
+  stream.pipe(csvFile);
+  
+  accountsLookup[file].forEach((account) => {
+    stream.write(account);
+  });
+  
+  stream.end();
+}
